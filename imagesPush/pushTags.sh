08e0350bd2f1a41179d36c0a7c2683f73a2ff9b8
shiptest-rc-me <shiptest.rc.me@gmail.com>
2017-02-16 12:05:51 +0530

    Update pushTags.sh

--- imagesPush/pushTags.sh
+++ imagesPush/pushTags.sh
@@ -32,7 +32,7 @@ set_context() {
  # echo "RES_VER_UP=$RES_VER_UP"
  # echo "RES_DH_UP=$RES_DH_UP"
  # echo "RES_DH_INT_STR=$RES_DH_INT_STR"
-  echo "RES_REPO_UP=$RES_REPO_UP"
+  echo "VAN_RES_REPO_UP=$VAN_RES_REPO_UP"
   echo "VAN_RES_REPO_STATE=$VAN_RES_REPO_STATE"
 
   echo "VAN_REPO_RESO_VERSION=$VAN_VERSION"
