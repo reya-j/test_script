e48a3397e25fd80d663b2f4f210d0514b5b1cf81
shiptest-rc-me <shiptest.rc.me@gmail.com>
2017-02-21 13:37:31 +0530

    Update pushTags.sh

--- imagesPush/pushTags.sh
+++ imagesPush/pushTags.sh
@@ -137,6 +137,7 @@ create_out_state() {
   echo "Creating a state file for $RES_IMG_OUT_UP"
   echo "MY_REPO_RESO_VERSION=$VAN_VERSION"
   echo "MY_COMMIT=$MY_REPO_COMMIT"
+  echo "MY_J_STATE=$JOB_STATE"
   echo versionName=$VAN_VERSION > "$JOB_STATE/$RES_IMAGE_OUT.env"
   echo commitSHA=$MY_REPO_COMMIT >> "$JOB_STATE/$RES_IMAGE_OUT.env"
 
